41ab645c12e905eb0d072f53261aeec12da28d0b
mohamednaeemm <naemm2048@gmail.com>
2025-04-24 19:17:25 +0200

    make hard coded todo list's date get today's date and hide completed section for now

--- src/modules/todayDate.js
+++ src/modules/todayDate.js
@@ -1,5 +1,14 @@
 export function todayDate() {
-        const dateInput = document.getElementById('date');
-        const today = new Date().toISOString().split('T')[0];
-        dateInput.value = today;
+    const dateElements = document.getElementsByClassName('date');
+    const date = new Date();
+    
+    const dateInput = document.getElementById('date');
+    const today = new Date().toISOString().split('T')[0];
+    dateInput.value = today;
+    const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    for (let element of dateElements) {
+        element.textContent = formattedDate;
+    }
 }
+
+document.addEventListener('DOMContentLoaded', todayDate);
